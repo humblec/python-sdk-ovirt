45daed5cfa96e18d9b21e985755c8bb0a7e9821e
Humble Devassy Chirammal <humble.devassy@gmail.com>
2013-10-13 23:00:00 +0530

    Update start_all_down_vms.py

--- start_all_down_vms.py
+++ start_all_down_vms.py
@@ -16,6 +16,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+# This program can be used to start all vms in a dc
+
 
 # Please make sure you are changing below parameters in api :
 
